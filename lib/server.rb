6a69a2b11e92587a2481faccadbb1400ccc2656b
cross-ts <ts.kouhei@gmail.com>
2019-01-14 14:27:46 +0900

    Implement server used socket

--- lib/server.rb
+++ lib/server.rb
@@ -8,15 +8,32 @@ class Server
   end
 
   def initialize
-    @socket = TCPServer.new 2000
+    Signal.trap('INT') { @killed = true }
+    @socket = create_server_socket
   end
 
   def run
+    client, addr_info = @socket.accept
+    client.puts "Hello!"
+    client.puts "Time is #{Time.now}"
     loop do
-      client = @socket.accept
-      client.puts "Hello!"
-      client.puts "Time is #{Time.now}"
-      client.close
+      sleep(1)
+      @x ||= 0
+      client.puts @x += 1
+      break if @killed
     end
+  ensure
+    @socket.close
+    client.close
+  end
+
+  private
+
+  def create_server_socket
+    socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
+    sock_addr = Socket.sockaddr_in(2000, '0.0.0.0')
+    socket.bind(sock_addr)
+    socket.listen(1)
+    socket
   end
 end
